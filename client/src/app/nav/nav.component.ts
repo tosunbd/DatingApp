a22cffd52f91607d114c96774ac5d01a90757003
taufiq <tosunbd@gmail.com>
2020-11-17 17:22:21 +0600

    5. Injecting Service

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  login(){
+    this.accountService.login(this.model)
+  }
+
+}
