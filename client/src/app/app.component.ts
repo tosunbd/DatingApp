583bf4ffb281a19792ce8a047ef040aa828713b7
taufiq <tosunbd@gmail.com>
2020-11-19 17:19:24 +0600

    parent to child

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,6 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private accountService: AccountService) { }
   // tslint:disable-next-line:typedef
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -24,13 +23,4 @@ export class AppComponent implements OnInit {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
   }
-
-  // tslint:disable-next-line:typedef
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-      }, error => {
-        console.log(error);
-      });
-    }
-  }
+}
